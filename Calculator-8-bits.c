1b58c482bbb9d9ca31665ee00e73f6208da38b44
brahianf <brahianf@unicauca.edu.co>
2019-09-13 00:11:27 -0500

    configuration registers, variables and start BCD

--- Calculator-8-bits.c
+++ Calculator-8-bits.c
@@ -10,9 +10,9 @@
  *
  *		 PORTB - INPUT 8 bits  
  * 
- *       PORTA - Display 1  (units), 
+ *       PORTA - Display 1  (hundreds)
  *       PORTC - Display 2  (tens), 
- *       PORTD - Display 3  (hundreds).
+ *       PORTD - Display 3  (units)
 ;		 RD7   - Display 4  (negative sign).
 ;
 ; Autor:	 Brahian Felipe Hurtado Erazo  TMS  
@@ -33,3 +33,25 @@
 
 void show_number(int num);
 int table_display(int c);
+
+
+void main() {
+    
+        int num1;                                       //var 1 number
+        int num2;                                       //var 2 number
+        
+        ANSEL=0;        
+        ANSELH=0;                                       //INPUT  digital
+        
+        TRISA=0;                                         //PORTA output (hundreds)
+        TRISC=0b10000000;                                //PORTC output (tens)),  RC7 Push Button new operation
+        TRISD=0;                                         //PORTD output ((units))
+        
+        TRISB=0b11111111;                               //PORTB input dip switch 8    
+        TRISE=0b00001111;                                //RE0,RE1,RE2 Push Button 1,2,3.
+                    
+        PORTA= 0x7E;                                     // Displays started "0".
+        PORTC= 0x7E;
+        PORTD= 0x7E;
+
+}
