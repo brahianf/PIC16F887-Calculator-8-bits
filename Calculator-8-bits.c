ba8fde35ef338fb5c1e28db38819a1c81f19d731
brahianf <brahianf@unicauca.edu.co>
2019-09-13 14:38:24 -0500

    Implementation function table for numbers Deco BCD

--- Calculator-8-bits.c
+++ Calculator-8-bits.c
@@ -56,16 +56,58 @@ void main() {
         
 }
 
-void show_number(int num){                          //Implementation function
+void show_number(int num){                          //Implementation function show_number
     
     int un,dec,cen;                                 //Declare var un, dec, cen
       
-    cen=(int)((num/100));  
-        PORTA=tabla_display(cen);
+    cen=(int)((num/100));                           //Split digits number
+        PORTA=table_display(cen);
     
     dec=(int)((num-(cen*100))/10);
-        PORTC=tabla_display(dec);
+        PORTC=table_display(dec);
         
     un=(int)(num%10);
-        PORTD=tabla_display(un);
+        PORTD=table_display(un);
+}
+
+int table_display(int c){                           //Implementation function 
+                                                    //this function returns the number to turn on the segments of each number for show un Deco BCD
+
+    int number;                                     
+    
+    switch(c){
+        
+        case 0:                   
+            number=0x7E;            // OUTPUT for number "0".
+            break;
+        case 1:                 
+            number= 0x30;          // OUTPUT for number "1".
+            break;
+        case 2:          
+            number= 0x6D;          //  OUTPUT for number "2".
+            break;
+        case 3:               
+            number=0x79;          // OUTPUT for number "3".
+            break;
+        case 4:                
+            number=0x33;         //  OUTPUT for number "4".
+            break;
+        case 5:                
+            number=0x5B;         // OUTPUT for number "5".
+            break;
+        case 6:                
+            number=0x5F;         // OUTPUT for number "6".
+            break;
+        case 7:                  
+            number=0x70;         //  OUTPUT for number "7".
+            break;
+        case 8:               
+            number=0x7F;			// OUTPUT for number "8".
+            break;
+        case 9:                  
+            number=0x73;         //  OUTPUT for number "9". 
+            break;  
+    }
+    return number;
+  
 }
