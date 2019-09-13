2687a4aeeb1af5b02900cb653592cc4675a3dde2
brahianf <brahianf@unicauca.edu.co>
2019-09-12 23:43:09 -0500

    Folders!

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Calculator-8-bits.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Calculator-8-bits.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=calculator-8-bits.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/calculator-8-bits.tar
